70c505c64518bb7052f8e26e30c58733249c5a2d
Shawn <shawn@enclave.vn>
2020-05-11 10:08:36 +0700

    Get tasks by week number & apply some hooks

--- packages/next-web/containers/TaskList/TaskList.tsx
+++ packages/next-web/containers/TaskList/TaskList.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { TabView, TabPanel } from 'primereact/tabview';
-import { ScrollPanel } from 'primereact/scrollpanel';
-import { ListBox } from 'primereact/listbox';
-import { GET_ALL_TASKS } from '../../graphql/query/task.query';
-import { Task } from '../../types';
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { TabView, TabPanel } from "primereact/tabview";
+import { ScrollPanel } from "primereact/scrollpanel";
+import { ListBox } from "primereact/listbox";
+import {
+  GET_ALL_TASKS,
+  GET_TASKS_BY_WEEKNO,
+} from "../../graphql/query/task.query";
+import { Task } from "../../types";
 
 type Props = {};
 
@@ -14,20 +17,33 @@ type SearchType = {
 };
 
 const TaskList: React.FC<Props> = (props) => {
-  const { data, error } = useQuery(GET_ALL_TASKS, { variables: { _id: '1' } });
-  const taskList: [SearchType] = data?.Task?.map((task: Task) => ({
+  const [selectedTasks, setSelectedTasks] = useState(["3"]);
+
+  const { data, error } = useQuery(GET_TASKS_BY_WEEKNO, {
+    variables: { weekNo: "202019" },
+  });
+  const weeklyTaskList = data?.TaskList[0];
+  const tasks = data?.TaskList[0]?.tasks;
+  const taskUIList: [SearchType] = tasks?.map((task: Task) => ({
     label: task?.title,
     value: task?._id,
   }));
+
+  console.log(selectedTasks);
   return (
     <TabView renderActiveOnly={true}>
-      <TabPanel header='Task List' leftIcon='pi pi-tags'>
-        <ScrollPanel style={{ width: '100%', height: '30vh' }}>
+      <TabPanel header="Task List" leftIcon="pi pi-tags">
+        <ScrollPanel style={{ width: "100%", height: "30vh" }}>
+          <p>Week: {weeklyTaskList?.weekNo}</p>
+          <p>Total: {weeklyTaskList?.total}</p>
+          <p>Completed: {weeklyTaskList?.completed}</p>
           <ListBox
             filter={true}
-            filterPlaceholder='Find'
+            filterPlaceholder="Find"
             multiple={true}
-            options={taskList}
+            options={taskUIList}
+            value={selectedTasks || 0}
+            onChange={(e) => setSelectedTasks(e.value)}
           />
         </ScrollPanel>
       </TabPanel>
--- packages/next-web/graphql/query/task.query.ts
+++ packages/next-web/graphql/query/task.query.ts
@@ -1,10 +1,24 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export const GET_ALL_TASKS = gql`
   query getAllTasks($_id: String!) {
     Task(_id: $_id) {
       _id
       title
+    }
+  }
+`;
+
+export const GET_TASKS_BY_WEEKNO = gql`
+  query getTasksByWeekNo($weekNo: String!) {
+    TaskList(weekNo: $weekNo) {
+      weekNo
+      total
+      completed
+      tasks {
+        _id
+        title
+      }
     }
   }
 `;
