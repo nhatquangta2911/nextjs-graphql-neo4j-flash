3932a337beabd10a3ad969d41099b36394a80063
nhatquangta2911 <nhatquangta2911@gmail.com>
2020-05-21 17:14:31 +0700

    Fix no data in a few sections

--- packages/next-web/containers/Tracking/Tracking.tsx
+++ packages/next-web/containers/Tracking/Tracking.tsx
@@ -3,6 +3,7 @@ import { TabPanel, TabView } from "primereact/tabview";
 import { ScrollPanel } from "primereact/scrollpanel";
 import GithubCalendar from "react-github-calendar";
 import ReactTooltip from "react-tooltip";
+import { Spinner } from "components";
 
 type Props = {
   github: string;
@@ -17,6 +18,13 @@ const Tracking: React.FC<Props> = (props) => {
         leftIcon="pi pi-chart-bar"
         contentStyle={{ width: "100%", height: "100%" }}
       >
+        {!github ||
+          (github === "" && (
+            <Spinner
+              small
+              message="Something went wrong. Please try loading the page again."
+            />
+          ))}
         <GithubCalendar
           username={github}
           color="hsl(203, 82%, 33%)"
@@ -25,17 +33,19 @@ const Tracking: React.FC<Props> = (props) => {
           <ReactTooltip delayShow={50} html />
         </GithubCalendar>
       </TabPanel>
-      <TabPanel header="Book Tracking" leftIcon="pi pi-calendar">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iure
-        labore quasi maxime enim fugiat eligendi quidem illo laborum. Sed
-        quibusdam ullam ex. Aspernatur nulla autem maiores, impedit pariatur
-        labore!
+      <TabPanel
+        header="Health Tracking"
+        leftIcon="pi pi-calendar"
+        contentStyle={{ width: "100%", height: "100%" }}
+      >
+        <Spinner small message="Update soon" />
       </TabPanel>
-      <TabPanel header="Financial Management" leftIcon="pi pi-list">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iure
-        labore quasi maxime enim fugiat eligendi quidem illo laborum. Sed
-        quibusdam ullam ex. Aspernatur nulla autem maiores, impedit pariatur
-        labore!
+      <TabPanel
+        header="Financial Management"
+        leftIcon="pi pi-list"
+        contentStyle={{ width: "100%", height: "100%" }}
+      >
+        <Spinner small message="Update soon" />
       </TabPanel>
     </TabView>
   );
